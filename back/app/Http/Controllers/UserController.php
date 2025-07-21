eec0ffbd94bd16fdd2bc1b010ea540b4a6fce353
stefanpetric <stefanpetric41@gmail.com>
2025-07-21 20:01:28 +0200

    Dodata logika za brisanje korisnika i za prikaz omiljenih kurseva ulogovanog korisnika

---
+++ back/app/Http/Controllers/UserController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Resources\KursResource;
+use App\Http\Resources\UserResource;
+use Illuminate\Support\Facades\Log;
+class UserController extends Controller
+{
+    public function destroy($id)
+    {
+        try {
+          
+            $user = Auth::user();
+            if(!$user->jeRole('admin')){
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Nemate prava da obrišete ovog korisnika.'
+                ], 403);
+            }
+
+
+            $user = User::findOrFail($id);
+            $user->kursevi()->delete();
+            $user->delete();
+            return response()->json([
+                'success' => true,
+                'message' => 'Korisnik i svi njegovi kursevi su obrisani.'
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Došlo je do greške prilikom brisanja korisnika i kurseva.',
+                'error' => $e->getMessage(),
+            ], 500);
+        }
+    }
+
+
+
+    public function getOmiljeniKursevi()
+{
+    try {
+        $user = Auth::user();
+        if(!$user->jeRole('student')){
+            return response()->json([
+                'success' => false,
+                'message' => 'Nemate prava da prikazete omiljene kurseve.'
+            ], 403);
+        }
+       
+        return KursResource::collection($user->omiljeniKursevi()->paginate(5));
+       
+    } catch (\Exception $e) {
+        return response()->json([
+            'success' => false,
+            'message' => 'Došlo je do greške prilikom dobijanja omiljenih kurseva.',
+            'error' => $e->getMessage(),
+        ], 500);
+    }
+}
+
+
+    
+}
--- back/routes/api.php
+++ back/routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\KursController;
 use App\Http\Controllers\CasController;
 use App\Http\Controllers\MaterijalController;
+use App\Http\Controllers\UserController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -29,4 +30,8 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/materijali/video/{id}', [MaterijalController::class, 'getVideo'])->name('materijal.video');
     Route::post('materijali',[MaterijalController::class,'store']);
 
+
+    
+    Route::delete('users/{id}', [UserController::class, 'destroy']);
+    Route::get('/users/omiljeni-kursevi', [UserController::class, 'getOmiljeniKursevi']);
 });
